228a334411fef4c629fa0a7768c6493b49b594c9
Jason Scheirer <jason.scheirer@gmail.com>
2014-11-12 09:56:42 -0800

    Use idiomatic Python

--- Search_Cursor_Demo.py
+++ Search_Cursor_Demo.py
@@ -1,9 +1,11 @@
 """ Demonstrate usage of the DA module's SearchCursor with a SQL statement. """
 
-import arcpy, os
+import os
+
+import arcpy
 
 ## Parameters
-feature_class = os.getcwd() + r"\Cursor Demo.gdb\Animals"
+feature_class = os.path.join(os.getcwd(), "Cursor Demo.gdb", "Animals")
 field_names   = ["OID@", "AnimalName", "AnimalType"]
 sql_statement = "ScarinessRank = 5"
 
@@ -11,5 +13,5 @@ sql_statement = "ScarinessRank = 5"
 with arcpy.da.SearchCursor(feature_class, field_names, sql_statement) as sc:
     print "The animals with a scariness rank of 5 are..."
     for row in sc:
-        print "OID: {0}  -- {1} the {2}".format(row[0], row[1], row[2])
+        print "OID: {0}  -- {1} the {2}".format(*row)
 
